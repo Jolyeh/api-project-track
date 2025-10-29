3461927b7af02c5921c65bf99a8d64ed7ff3ecd7
jolyeh <yehezekocolojoelprince@gmail.com>
2025-10-29 17:04:56 +0100

    deployement

--- src/generated/prisma/edge.js
+++ src/generated/prisma/edge.js
@@ -305,7 +305,6 @@ const config = {
     "db"
   ],
   "activeProvider": "mysql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- src/server.js
+++ src/server.js
@@ -7,5 +7,3 @@ const HOST = "0.0.0.0";
 app.listen(PORT, HOST, () => {
   console.log(`Server is running on http://${HOST}:${PORT}`);
 });
-
-//erg
